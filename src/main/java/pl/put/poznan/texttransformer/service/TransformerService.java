84b08733e1835a081e528d7173ac6981c6bc6d5b
Marcin Krzewinski <marcin.krzewinski@proton.me>
2026-05-14 22:42:56 +0200

    update UML and TranformerService to include logger in the base class

--- src/main/java/pl/put/poznan/texttransformer/service/TransformerService.java
+++ src/main/java/pl/put/poznan/texttransformer/service/TransformerService.java
@@ -1,9 +1,12 @@
 package pl.put.poznan.texttransformer.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public abstract class TransformerService {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
     public abstract String getName();
     public abstract String transform(String text);
 }
