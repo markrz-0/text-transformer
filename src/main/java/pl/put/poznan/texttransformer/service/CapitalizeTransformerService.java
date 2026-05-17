887aa61e9d447055e718d6bef53183b5cbf55de5
Antoni Świderski <antoni.swiderski25@gmail.com>
2026-05-17 21:02:03 +0200

    chore cleanup comments

--- src/main/java/pl/put/poznan/texttransformer/service/CapitalizeTransformerService.java
+++ src/main/java/pl/put/poznan/texttransformer/service/CapitalizeTransformerService.java
@@ -7,8 +7,7 @@ public class CapitalizeTransformerService extends TransformerService {
 
     @Override
     public String getName() {
-        // Zwracamy nazwę zgodną z wymaganiami w backlogu
-        return "capitalize"; 
+        return "capitalize";
     }
 
     @Override
@@ -18,19 +17,15 @@ public class CapitalizeTransformerService extends TransformerService {
         }
 
         char[] chars = text.toCharArray();
-        boolean capitalizeNext = true; // Flaga mówiąca, czy następny znak ma być wielką literą
+        boolean capitalizeNext = true;
 
         for (int i = 0; i < chars.length; i++) {
-            // Jeśli znak to spacja (lub inny biały znak), kolejne słowo musi zacząć się z wielkiej litery
             if (Character.isWhitespace(chars[i])) {
                 capitalizeNext = true;
             } else if (capitalizeNext) {
-                // Zamieniamy na wielką literę i opuszczamy flagę
                 chars[i] = Character.toUpperCase(chars[i]);
                 capitalizeNext = false;
             } else {
-                // Opcjonalnie: upewniamy się, że reszta liter w słowie jest mała 
-                // (zależy od ustaleń w zespole, ale zazwyczaj "capitalize" tak działa)
                 chars[i] = Character.toLowerCase(chars[i]);
             }
         }
